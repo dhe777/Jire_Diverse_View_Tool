08986b8e84449b4f8427101b38244915eaccb9df
dhe777 <33729890+dhe777@users.noreply.github.com>
2018-03-14 15:31:52 -0600

    first commit

---
+++ lib/jira_diverse_view_tool.rb
@@ -0,0 +1,6 @@
+require "jira_diverse_view_tool/version"
+require "jira_diverse_view_tool/renderer"
+
+module JiraDiverseViewTool
+  # Your code goes here...
+end
---
+++ lib/jira_diverse_view_tool/version.rb
@@ -0,0 +1,3 @@
+module JiraDiverseViewTool
+  VERSION = "0.1.0"
+end
